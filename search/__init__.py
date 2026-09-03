0b293b0d260e3899472a0ebafecf16a6d83764e6
Simran Goyal <simrangoyal417@gmail.com>
2026-09-03 20:59:04 +0530

    Revert "Add genuine public web search integration"
    This reverts commit 233e8c8423f9639ca5b151741d413c12c352d31a.

--- search/__init__.py
+++
@@ -1,8 +0,0 @@
-from .web_search import SearchConfigurationError, SearchError, download_candidate_images, search_public_images
-
-__all__ = [
-    "SearchConfigurationError",
-    "SearchError",
-    "download_candidate_images",
-    "search_public_images",
-]
